8652210004b9414df3803522ebe176a4f9626db6
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-08-27 21:12:04 +0200

    aggiunta gestione degli errori

--- script.js
+++ script.js
@@ -1,73 +1,63 @@
 const productId = new URLSearchParams(window.location.search).get("productID");
-
 const URL = productId
   ? "https://striveschool-api.herokuapp.com/api/product/" + productId
   : "https://striveschool-api.herokuapp.com/api/product/";
 
-const handleSubmit = async (event) => {
-  event.preventDefault();
+const container = document.querySelector(".container .row");
 
-  const product = {
-    name: document.getElementById("name").value,
-    description: document.getElementById("description").value,
-    brand: document.getElementById("brand").value,
-    imageUrl: document.getElementById("imageUrl").value,
-    price: document.getElementById("price").value,
-  };
-
-  console.log(product);
-
-  try {
-    const response = await fetch(URL, {
-      method: productId ? "PUT" : "POST",
-      body: JSON.stringify(product),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjQwYWMwMzRmZjAwMTQwM2Y1NmYiLCJpYXQiOjE2OTI5NTE1NjIsImV4cCI6MTY5NDE2MTE2Mn0.WzUEH9dUMb6Rhhvrm-VSLjHN5UujsM0u6efWoRms0A4",
-      },
-    });
-
-    if (response.ok) {
-      const sendedProduct = await response.json();
-      console.log("inviato con successo ", sendedProduct);
-
-      document.getElementById("name").value = "";
-      document.getElementById("description").value = "";
-      document.getElementById("brand").value = "";
-      document.getElementById("imageUrl").value = "";
-      document.getElementById("descImg").value = "";
-      document.getElementById("price").value = "";
-    }
-  } catch (error) {
-    console.log(error);
-  }
+const redirectMe = () => {
+  const timer = setTimeout(function () {
+    window.location.href = "index.html";
+    window.clearTimeout(timer);
+  }, 1000);
 };
 
-const loadData = async () => {
-  try {
-    const response = await fetch(URL, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjQwYWMwMzRmZjAwMTQwM2Y1NmYiLCJpYXQiOjE2OTI5NTE1NjIsImV4cCI6MTY5NDE2MTE2Mn0.WzUEH9dUMb6Rhhvrm-VSLjHN5UujsM0u6efWoRms0A4",
-      },
-    });
-
-    const product = await response.json();
-    document.getElementById("name").value = `${product.name}`;
-    document.getElementById("description").value = `${product.description}`;
-    document.getElementById("brand").value = `${product.brand}`;
-    document.getElementById("imageUrl").value = `${product.imageUrl}`;
-    document.getElementById("price").value = `${product.price}`;
-  } catch (error) {
-    console.log(error.message);
-  }
+const clearInput = () => {
+  document.getElementById("name").value = "";
+  document.getElementById("description").value = "";
+  document.getElementById("brand").value = "";
+  document.getElementById("imageUrl").value = "";
+  document.getElementById("price").value = "";
 };
 
-const deleteProduct = () => {
-  fetch(URL, {
+const successAdded = () => {
+  document.querySelector("form .alert-success").classList.remove("d-none");
+
+  const remove = setTimeout(function () {
+    document.querySelector("form .alert-success").classList.add("d-none");
+    window.clearTimeout(remove);
+  }, 2000);
+};
+
+const getResponse = () => {
+  const response = fetch(URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjQwYWMwMzRmZjAwMTQwM2Y1NmYiLCJpYXQiOjE2OTI5NTE1NjIsImV4cCI6MTY5NDE2MTE2Mn0.WzUEH9dUMb6Rhhvrm-VSLjHN5UujsM0u6efWoRms0A4",
+    },
+  });
+
+  return response;
+};
+
+const formResponse = async (product) => {
+  response = fetch(URL, {
+    method: productId ? "PUT" : "POST",
+    body: JSON.stringify(product),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjQwYWMwMzRmZjAwMTQwM2Y1NmYiLCJpYXQiOjE2OTI5NTE1NjIsImV4cCI6MTY5NDE2MTE2Mn0.WzUEH9dUMb6Rhhvrm-VSLjHN5UujsM0u6efWoRms0A4",
+    },
+  });
+
+  return response;
+};
+
+const deleteProduct = async () => {
+  await fetch(URL, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
@@ -75,21 +65,20 @@ const deleteProduct = () => {
         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGU4NjQwYWMwMzRmZjAwMTQwM2Y1NmYiLCJpYXQiOjE2OTI5NTE1NjIsImV4cCI6MTY5NDE2MTE2Mn0.WzUEH9dUMb6Rhhvrm-VSLjHN5UujsM0u6efWoRms0A4",
     },
   });
+
+  clearInput();
+  document.querySelector(".modal-body").style.color = "#008000";
+  document.querySelector(".modal-body").innerHTML = "Prodotto eliminato con successo!";
+  redirectMe();
+  document.querySelector(".modal-footer").innerHTML = "";
 };
 
-window.onload = async () => {
-  const submitBtn = document.querySelector("button[type='submit']");
-
-  if (productId) {
-    loadData();
-    submitBtn.innerHTML = "Modifica";
-    document.querySelector("form").innerHTML += `<button  class="btn btn-danger" >Elimina</button>`;
-    document.querySelector(
-      "form"
-    ).innerHTML += `<a href="index.html" class="btn btn-secondary ms-1" >Torna indietro</button>`;
-    const deleteBtn = document.querySelector(".btn-danger");
-    deleteBtn.onclick = () => {
-      deleteProduct();
-    };
+const checkStatus = (status) => {
+  if (status === 401) {
+    throw new Error(status + "- C'è stato un problema nell'autenticazione...");
+  } else if (status === 405) {
+    throw new Error(status + "- Ci dispiace, non riusciamo a trovare la pagina richiesta...");
+  } else if (status > 500) {
+    throw new Error("Ci dispiace, al momento abbiamo alcuni problemi interni col server...");
   }
 };
